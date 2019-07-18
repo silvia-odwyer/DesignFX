18df794bb8e177e093c28c1406e5749ac36acbb1
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2019-07-18 21:39:09 +0100

    Add gitignore.

---
+++ src/components/Landing.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <div>
+      <h1>Graphic Design App</h1>
+      <p>A decentralized graphic design app built on <a href="https://blockstack.org" target="_blank">Blockstack</a></p>
+      <button class="btn btn-default bg-white" @click.prevent="signIn">Sign In With Blockstack</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { userSession } from '../userSession'
+
+export default {
+  name: 'landing',
+  methods: {
+    signIn () {
+      userSession.redirectToSignIn()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
