39b44c3e3c4018b60e427891eb1187545e3b117c
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2019-07-22 23:18:18 +0100

    Added Designs component.

---
+++ src/components/Designs.vue
@@ -0,0 +1,72 @@
+<template>
+    <ul>
+          
+    <h3>Design Templates</h3>
+    <ul>
+        <li v-on:click="addRectangle('red')">Add Red Rectangle</li>
+        <li v-on:click="addRectangle('green')">Add Green Rectangle</li>
+        <li v-on:click="addText()">Add Text</li>
+        <li v-on:click="addImage(layout_imgs[0])">Add Image</li>
+        <li v-on:click="save()">Save</li>
+        <li v-on:click="clear()">Clear</li>
+        <li v-for="img in layout_imgs" v-on:click="displayLayout(img)">
+        <p>{{img.layout_name}}</p>
+        <img :src="img.img_src" height="200" width="300">
+        </li>
+    </ul>              
+    </ul>
+</template>
+
+<script>
+/* eslint-disable */
+import { userSession } from '../userSession'
+import image from "@/assets/daisies_small.jpg";
+import layout1 from "@/assets/travel.png";
+import layout2 from "@/assets/lemonade.png";
+import layout3 from "@/assets/summer_collection.png";
+import layout4 from "@/assets/travel.png";
+import layout5 from "@/assets/lemonade.png";
+import layout6 from "@/assets/summer_collection.png";
+
+export default {
+  name: 'designs',
+  props: ['user'],
+  data () {
+    return {
+    layout_imgs: [{img_src: layout1, layout_name: "Travel"}, {img_src: layout2, layout_name: "Lemonade"}, 
+      {img_src: layout3,  layout_name: "Summer Collection"}],
+    img: null
+    }
+  },
+  methods: {
+    displayLayout(img) {
+      const image = new window.Image();
+      image.src = img.img_src;
+      image.onload = () => {
+        // set image only when it is loaded
+        this.image_template = {image: image, draggable: true, name: img.layout_name};
+      };
+    },
+    fetchData () {
+      userSession.getFile(STORAGE_FILE) // decryption is enabled by default
+        .then((todosText) => {
+          var notes = JSON.parse(todosText || '[]')
+          notes.forEach(function (note, index) {
+            note.id = index
+          })
+          this.uidCount = notes.length
+          this.notes = notes
+        })
+    },
+
+    signOut () {
+      userSession.signUserOut(window.location.href)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
